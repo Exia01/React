df5bcdf2eea55cf65e848a8881ead9f69cc1b4ec
Jose <sixgg4@gmail.com>
2018-10-22 16:23:15 -0400

    finished lsson 15

--- firstApp/client/src/components/NinjasComponent/Ninjas.js
+++ firstApp/client/src/components/NinjasComponent/Ninjas.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
-import './Ninjas.css';
-/* We can automatically pass down the props from parent to child. */
-class Ninjas extends Component {
-    render() {
-        /*  console.log(this.props); //this logs an object */
-        /*   'Destructuring' We extracted the properties from the props object; can be used wherever */
-        const { people } = this.props
-        /*     It makes it easier to assign and it also makes it reusable */
-        /* Here we use map which creates a new element  */
-        const peopleList = people.map(person => {
-            //console.log(person)
-            return (
-                <div className="ninja" key={person.id}>
-                    <ul>
-                        <li>Name: {person.name}</li>
-                        <li><p>Age: {person.age}</p></li>
-                        <li><p> Language: {person.language}</p></li>
-                    </ul>
-                </div>
-            )
-        })
+import React from 'react';
+
+/* We are changing this to a UI Container since there are no states, it is only related to UI */
+//we still need to pass the 'properties' ninja
+ /*  'Destructuring' We extracted the properties from the props object; can be used wherever */
+const Ninjas = ({ people }) => {
+    /* Here we use map which creates a new element  */
+    const peopleList = people.map(person => {
+        //console.log(person)
         return (
-            <div className="ninja-list">
-                {peopleList}
+            <div className="ninja" key={person.id}>
+                <ul>
+                    <li>Name: {person.name}</li>
+                    <li><p>Age: {person.age}</p></li>
+                    <li><p> Language: {person.language}</p></li>
+                </ul>
             </div>
         )
-    }
+    })
+    return (
+        <div className="ninja-list">
+            {peopleList}
+        </div>
+    )
 }
 
 export default Ninjas;
