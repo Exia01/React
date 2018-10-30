8bde98e1a8b792b01309462cc341b53baa853530
Jose <sixgg4@gmail.com>
2018-10-30 12:00:12 -0400

    lssn 32

--- NetNinja/poketimes/src/App.js
+++ NetNinja/poketimes/src/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
-/* Using BrowserRouter instead of HashRouter */
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+/* Using BrowserRouter instead of HashRouter. Switch renders one route at a time */
 import './App.css';
 /* Props are being passed via "Route" */
 import About from './components/AboutComponent/About';
@@ -14,12 +14,13 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <Navbar/>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/:post_id" component={Post} />
-          
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
