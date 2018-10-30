95534346011e8c3367a25786f22f437d47e799a7
Jose <sixgg4@gmail.com>
2018-10-30 14:44:28 -0400

    added redux

---
+++ NetNinja/poketimes/src/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+/* Initial State that we will pass to the rootReducer */
+const initState = {
+  post: []
+};
+
+/* creating the Reducer with the initial state */
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+
+export default rootReducer
