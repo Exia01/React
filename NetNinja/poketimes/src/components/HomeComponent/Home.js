b7327c179935b89ceeebf520750574e436a57aef
Jose <sixgg4@gmail.com>
2018-10-30 12:19:15 -0400

    lssn 33

--- NetNinja/poketimes/src/components/HomeComponent/Home.js
+++ NetNinja/poketimes/src/components/HomeComponent/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Pokeball from '../img/pokeball.png'
+
 import { Link } from 'react-router-dom';
 /* This is a class based Component*/
 class Home extends Component {
@@ -36,9 +38,10 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A Pokeball"/>
             <div className="card-content">
               <Link to={'/' + post.id}>
-              <span className="card-title">{post.title}</span>
+              <span className="card-title red-text">{post.title}</span>
               </Link>
               <p>{post.body}</p>
             </div>
@@ -49,7 +52,7 @@ class Home extends Component {
       <div className="center">No posts yet</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
