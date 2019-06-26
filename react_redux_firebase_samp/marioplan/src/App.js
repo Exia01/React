8748dadd3fcd5072230346bb112b395a03254873
Jose <sixgg4@gmail.com>
2019-06-25 20:11:45 -0400

    setup components

--- react_redux_firebase_samp/marioplan/src/App.js
+++ react_redux_firebase_samp/marioplan/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/layout/Navbar'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
   // BrowserRouter enables links
   return (
     <BrowserRouter>
-      <Navbar/>
       <div className="App">
-        <h1>Hello!</h1>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails}/>
+        </Switch>
       </div>
     </BrowserRouter>
   );
