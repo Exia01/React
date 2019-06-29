6ed56539c40b1eae10af1d06619ff2bfd0b92ab9
Jose <sixgg4@gmail.com>
2019-06-29 15:34:00 -0400

    setup project list and project details

--- react_redux_firebase_samp/marioplan/src/components/projects/ProjectDetails.js
+++ react_redux_firebase_samp/marioplan/src/components/projects/ProjectDetails.js
@@ -1,22 +1,49 @@
 import React from 'react'
+//import connect and redux state
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 //props coming from the router
 const ProjectDetails = (props) => {
-    console.log(props)
-    const id = props.match.params.id;
+  const { project } = props;  //destructuring
+  if (project) {
     return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Project title - {id}</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et labore quaerat quibusdam vel saepe, ab voluptate accusantium culpa nemo fuga earum? Soluta amet nobis officia sed neque fuga aperiam quia?</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by The Net Ninja</div>
-                    <div>2nd September, 2am</div>
-                </div>
-            </div>
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+            <div>2nd September, 2am</div>
+          </div>
         </div>
+      </div>
     )
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
+    )
+  }
 }
+//functions defines what we want to attach to our props
+const mapStateToProps = (state, ownProps) => {
+    // console.log(state); 
+    const id = ownProps.match.params.id;// getting the props from component as ownProps
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null // if project find id
+    return {
+      project: project
+    }
+  }
 
-export default ProjectDetails
+export default compose( //compose the higher order components
+    connect(mapStateToProps),
+    firestoreConnect([{
+      collection: 'projects' // specifying which base we're using
+    }])
+  )(ProjectDetails)
+  
--- react_redux_firebase_samp/marioplan/src/components/projects/ProjectList.js
+++ react_redux_firebase_samp/marioplan/src/components/projects/ProjectList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 //single component being reused
 import ProjectSummary from './ProjectSummary';
+import { Link } from 'react-router-dom'
 
 //destructuring directly and grabbing project directly from the props
 const ProjectList = ({projects}) => {
   //using conditional truthy to render if projects and passing props
   return (
     <div className="project-list section">
-      { projects && projects.map(project => {
+      {projects && projects.map(project => {
         return (
-          <ProjectSummary project={project} key={project.id} />
+          <Link to={`/project/${project.id}`} key={project.id}>
+            <ProjectSummary project={project} />
+          </Link>
         )
-      })}  
+      })}
     </div>
   )
 }
