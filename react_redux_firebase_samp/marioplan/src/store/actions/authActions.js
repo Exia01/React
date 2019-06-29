964b4502b6b062b2507d6589a75acb028184afac
Jose <sixgg4@gmail.com>
2019-06-29 17:26:24 -0400

    conect to firebase reducer, implement create project action

---
+++ react_redux_firebase_samp/marioplan/src/store/actions/authActions.js
@@ -0,0 +1,16 @@
+export const signIn = (credentials) => {//this is the action creator
+  //returning a function with a third object and destructuring
+  return (dispatch, getState, {getFirebase}) => {
+    const firebase = getFirebase(); //initializing firebase instance
+    //
+    firebase.auth().signInWithEmailAndPassword(
+      credentials.email,
+      credentials.password
+    ).then(() => {
+      dispatch({ type: 'LOGIN_SUCCESS' });// just passing type and no additional data
+    }).catch((err) => {
+      dispatch({ type: 'LOGIN_ERROR', err });
+    });
+
+  }
+}
--- react_redux_firebase_samp/marioplan/src/store/reducers/authReducer.js
+++ react_redux_firebase_samp/marioplan/src/store/reducers/authReducer.js
@@ -1,8 +1,25 @@
-const initState = {}
+const initState = {
+  authError: null // if error it will change from the switchded
+}
 
 //the first time this reducer runs and does not have a state. Need to pass 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch(action.type){
+    case 'LOGIN_ERROR':
+      console.log(action.type.err);
+      return {
+        ...state,
+        authError: 'Login failed' //property
+      }
+    case 'LOGIN_SUCCESS':
+      console.log('login success');
+      return {
+        authError: null
+      }
+    default:
+      return state
+  }
 };
+
 
 export default authReducer;
