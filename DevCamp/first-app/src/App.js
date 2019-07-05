51f48bb6eda1cce5757e276c17306a08ce4dda90
Jose <sixgg4@gmail.com>
2019-07-05 17:40:05 -0400

    prop practice

--- DevCamp/first-app/src/App.js
+++ DevCamp/first-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There</h1>
+      <Person name="max" age="28"/>
+      <Person name="Manu" age="23" />
+      <Person name="Melanie" age="17">My hobbies: Gaming</Person>
     </div>
   );
 }
