5cc42f5733e68b31a80f54433d52911609143b52
Jose <sixgg4@gmail.com>
2019-07-07 17:20:18 -0400

    implement onChange

--- DevCamp/first-app/src/App.js
+++ DevCamp/first-app/src/App.js
@@ -11,28 +11,47 @@ class App extends Component {
       {name: "Max", age: "28"},
       {name: "Manu", age: "23"},
       {name: "Melanie", age: "17"}
-    ], 
-    otherState:"Some otherValue"
+    ],
+    otherState: "Some otherValue"
   }
-  switchNameHandler = (e) => { //class method, standard to use "handler at the end"
-    console.log(e)
-    e.preventDefault()
+  switchNameHandler = (newName) => { //class method, standard to use "handler at the end"
     // //DON'T DO THIS: this.state.persons[0].name = "test"
-    this.setState({ persons: [
-      {name: "Kyle", age: "28"},
-      {name: "Kevin", age: "26"},
-      {name: "Melanie", age: "17"}
-    ] })
+    this.setState({
+      persons: [
+        {name: newName, age: "28"},
+        {name: "Manu", age: "23"},
+        {name: "Melanie", age: "17"}
+      ]
+    })
+  }
+  nameChangedHandler = (e) => {
+    e.preventDefault()
+    this.setState({
+      persons: [
+        {name: 'Max', age: "28"},
+        {name: e.target.value, age: "23"},
+        {name: "Melanie", age: "17"}
+      ]
+    })
   }
   render() {
     return (
       <div className="App">
         <div>
           <h1>Hello There</h1>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          <button onClick={() => this.switchNameHandler("Josh")}>Switch Name</button>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            clickEvent={this.switchNameHandler.bind(this, "Yoshi")}
+            changed={this.nameChangedHandler}
+            > My Hobbies:Racing </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
         </div>
       </div>
     )
--- DevCamp/first-app/src/Person/Person.js
+++ DevCamp/first-app/src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Person = (props) => {
     return (
         <div>
-            <p>My name is {props.name} and i am {props.age} years old</p>
+            <p onClick={props.clickEvent}>My name is {props.name} and i am {props.age} years old</p>
             <p>{props.children}</p>
+            <input value={props.name} type="text" onChange={props.changed}/>
         </div>
     )
 }
