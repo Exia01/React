c2c32e9faaf6fe9718c44fe734ab1acfa3b163b6
Jose <sixgg4@gmail.com>
2019-07-08 19:05:03 -0400

    add class and css

--- DevCamp/first-app/src/Person/Person.js
+++ DevCamp/first-app/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.clickEvent}>My name is {props.name} and i am {props.age} years old</p>
             <p>{props.children}</p>
             <input value={props.name} type="text" onChange={props.changed}/>
