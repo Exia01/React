af652ae9ca39eab3b3a3c9347f2c65c981208803
Jose <gonzalezj@protonmail.com>
2019-07-08 22:06:47 -0400

    setup handle submit and inputComponent

--- DevCamp/first-app/src/App.js
+++ DevCamp/first-app/src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 import './App.css';
 import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+
 
 
 class App extends Component {
@@ -12,7 +15,8 @@ class App extends Component {
       {name: "Manu", age: "23"},
       {name: "Melanie", age: "17"}
     ],
-    otherState: "Some otherValue"
+    otherState: "Some otherValue",
+    inputValue: "test@value.com",
   }
   switchNameHandler = (newName) => { //class method, standard to use "handler at the end"
     // //DON'T DO THIS: this.state.persons[0].name = "test"
@@ -34,24 +38,57 @@ class App extends Component {
       ]
     })
   }
+  inputValueSubmitHandler = (obj) => {
+    this.setState({inputValue: obj})
+    console.log(this.props);
+    
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, prevState)
+    console.log(this.state)
+
+  }
   render() {
+    //inline style can be difficult for actions --> "hover" and can also help with scoping 
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <div>
-          <h1>Hello There</h1>
-          <button onClick={() => this.switchNameHandler("Josh")}>Switch Name</button>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            clickEvent={this.switchNameHandler.bind(this, "Yoshi")}
-            changed={this.nameChangedHandler}
-            > My Hobbies:Racing </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          <BrowserRouter>
+            <Route exact path='/' render={props =>
+              <div>
+                <h1>Hello There</h1>
+                <button style={style} onClick={() => this.switchNameHandler("Josh")}>Switch Name</button>
+                <Person
+                  name={this.state.persons[0].name}
+                  age={this.state.persons[0].age} />
+                <Person
+                  name={this.state.persons[1].name}
+                  age={this.state.persons[1].age}
+                  clickEvent={this.switchNameHandler.bind(this, "Yoshi")}
+                  changed={this.nameChangedHandler}
+                > My Hobbies:Racing </Person>
+                <Person
+                  name={this.state.persons[2].name}
+                  age={this.state.persons[2].age} />
+                <div className="test-links">
+                  <Link to="/input">Input</Link><br />
+                  <Link to="/output">Output</Link>
+                </div>
+              </div>
+            } />
+            <Route
+              path='/input'
+              render={(props) => <UserInput {...props} inputValueSubmitHandler={this.inputValueSubmitHandler} currentEmail={this.state.inputValue} />}
+            />
+            <Route path="/output" component={UserInput} />
+          </BrowserRouter>
         </div>
       </div>
     )
---
+++ DevCamp/first-app/src/UserInput/UserInput.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+class UserInput extends Component {
+  state = {
+    emailValue:[..."",this.props.currentEmail] //Not sure if need to spread before. Props are read only??
+  }
+  handleChange = (e) => {
+    this.setState({
+      emailValue: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      emailValue: ''
+    })
+    this.props.inputValueSubmitHandler(this.state.emailValue)
+   
+  }
+  render() {
+    const formStyle = {
+      width: "500px"
+    }
+    return (
+      <div>
+        <form style={formStyle} onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label >Email address</label>
+            <input type="email" className="form-control" id="email-input" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleChange} value={this.state.emailValue} />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default UserInput
+
+
+//router pass multiple props: https://tylermcginnis.com/react-router-pass-props-to-components/
+//props: https://flaviocopes.com/react-props/
