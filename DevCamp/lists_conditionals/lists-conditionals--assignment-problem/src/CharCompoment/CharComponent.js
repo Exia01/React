a1ef37e2aca1756a1213bc698494a7724e8bb189
Jose <sixgg4@gmail.com>
2019-07-11 17:20:12 -0400

    create component, add uuid

---
+++ DevCamp/lists_conditionals/lists-conditionals--assignment-problem/src/CharCompoment/CharComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharComponent = (props) => {
+    return (
+        <div>
+            <p>CharComponent works!</p>
+        </div>
+    )
+}
+
+export default CharComponent
