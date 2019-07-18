4cd49386575e79cb2c2c71d6e870432c6fa97781
Jose <sixgg4@gmail.com>
2019-07-18 16:38:00 -0400

    UseEffect and hooks

--- DevCamp/compoment_internal_deepDive/sample-app/src/components/Cockpit/Cockpit.js
+++ DevCamp/compoment_internal_deepDive/sample-app/src/components/Cockpit/Cockpit.js
@@ -1,29 +1,54 @@
-import React from 'react'
-import classes from './Cockpit.module.css' // css modules scoped to this component
+import React, { useEffect } from 'react';
+//useEffect is the second most important hook you can use next to useState
+//useEffect //combines the functionality of the use cases for lifecycle hooks
+import classes from './Cockpit.module.css'; // css modules scoped to this component
 
-const Cockpit = (props) => {
-    // console.log(props);
+const Cockpit = props => {
+  //useEffect executes for every render cycle can be tricky to use
+//   useEffect(() => {
+//     console.log('[Cockpit.js] UseEffect...');
+//     //http request can be made here
+//     setTimeout(() => {
+//       //simulating requests
+//       alert('Saved data to cloud');
+//     }, 1000);
+//   }, [props.persons]); //can pass second argument and points to all the variables that are actually used on the effect
     
-    //if 2 persons or less
-    //creating css styling to match app.css on if condition then joining
-    const assignClasses = []
-    let btnClass = ''
-    if (props.showPersons) { //coming from the app state 
-        btnClass = classes.Red // string from obj css loader that gives us access
-    }
-    if (props.persons.length <= 2) {
-        assignClasses.push(classes.red) // classes = ["red"]
-    }
-    if (props.persons.length <= 1) {
-        assignClasses.push(classes.bold)// classes = ["red", "bold"]
-    }
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <h1 className={assignClasses.join(" ")}>Hello There</h1>
-            <button className={btnClass} onClick={props.clicked}>Toggle Persons</button>
-        </div>
-    )
-}
+    
+//   //useEffect executes for every render cycle can be tricky to use
+  useEffect(() => {
+    console.log('[Cockpit.js] UseEffect...');
+    //http request can be made here
+    setTimeout(() => {
+      //simulating requests
+      alert('Saved data to cloud');
+    }, 1000);
+  }, []);//empty array tells react only to execute only when any dependencies changes and thus only once because it is empty
+  // console.log(props);
 
-export default Cockpit
+  //if 2 persons or less
+  //creating css styling to match app.css on if condition then joining
+  const assignClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    //coming from the app state
+    btnClass = classes.Red; // string from obj css loader that gives us access
+  }
+  if (props.persons.length <= 2) {
+    assignClasses.push(classes.red); // classes = ["red"]
+  }
+  if (props.persons.length <= 1) {
+    assignClasses.push(classes.bold); // classes = ["red", "bold"]
+  }
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <h1 className={assignClasses.join(' ')}>Hello There</h1>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
+};
+
+export default Cockpit;
