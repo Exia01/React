284c98487dc2c49d373c24bb3ad44130752a027c
Jose <sixgg4@gmail.com>
2019-07-18 15:49:09 -0400

    component update lifecyfle(for state changes)

--- DevCamp/compoment_internal_deepDive/sample-app/src/containers/App.js
+++ DevCamp/compoment_internal_deepDive/sample-app/src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // import {BrowserRouter, Link, Route} from 'react-router-dom';
 
 import Persons from '../components/Persons/Persons';
@@ -16,23 +16,38 @@ class App extends Component {
   //can implement from class based component
   state = {
     persons: [
-      { id: 1, name: 'Max', age: '28' },
-      { id: 2, name: 'Manu', age: '23' },
-      { id: '23', name: 'Melanie', age: '17' }
+      {id: 1, name: 'Max', age: '28'},
+      {id: 2, name: 'Manu', age: '23'},
+      {id: '23', name: 'Melanie', age: '17'}
     ],
     otherState: 'Some otherValue',
     showPersons: false
   };
+
   // after constructor runs this life cycle follows
   //need to add static to execute properly
   static getDerivedStateFromProps(props, state) {
     console.log('[App.js] getDerivedStateFromProps', props);
     return state;
   }
+
+
   //after getDerivedStateFromProps render gets executed
   componentDidMount() {
     console.log('[App.js] componentDidMount');
   } //after render componentDidMount runs
+
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('[App.js] shouldComponentUpdate')
+    return true // have to return either true or false. We can even compare props to determine that
+  }
+
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate')
+  }
+
   nameChangedHandler = (e, id) => {
     //event and id
     e.preventDefault();
@@ -50,7 +65,7 @@ class App extends Component {
     const persons = [...this.state.persons]; //spread operator to copy the state
     persons[personIndex] = person; // update the value on the array
 
-    this.setState({ persons: persons }); //can pass pass object persons for simplicity
+    this.setState({persons: persons}); //can pass pass object persons for simplicity
   };
 
   deletePersonHandler = personIndex => {
@@ -59,18 +74,14 @@ class App extends Component {
     // const persons = [...this.state.persons]//spreads the state into a new arr/
     //changing pointer since it is a const
     persons.splice(personIndex, 1); // returns the copy without that index removed
-    this.setState({ persons: persons });
+    this.setState({persons: persons});
   };
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     //inverting value if true. React will merge the rest of the components
-    this.setState({ showPersons: !doesShow });
+    this.setState({showPersons: !doesShow});
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    // console.log(prevProps, prevState)
-    // console.log(this.state)
-  }
   render() {
     console.log('[App.js] render');
     //default which checks every time the state renders or updates.
