d3b04f1da472726709faf3435e8f7cfe651e2311
Jose <sixgg4@gmail.com>
2019-07-22 20:06:07 -0400

    multiple changes
    Add container and component folder
    Add burger Comopent and subfolders

---
+++ DevCamp/Projects/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,23 @@
+import React, {Component, Fragment} from 'react' //using fragment instead of auxiliary hoc
+import Burger from '../../components/Burger/Burger'
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients:{
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
+    }
+    render() {
+        return (
+            <Fragment>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </Fragment>
+        )
+    }
+}
+
+export default BurgerBuilder
