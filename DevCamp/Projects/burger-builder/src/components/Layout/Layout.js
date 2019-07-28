a9a58741d0d5d92e18ab136ff4ec9e85aa64f4a8
Jose Gonzalez <Gonzalezj@protonmail.com>
2019-07-28 19:12:09 -0400

    added side bar and toggler handler

--- DevCamp/Projects/burger-builder/src/components/Layout/Layout.js
+++ DevCamp/Projects/burger-builder/src/components/Layout/Layout.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../hoc/Auxiliary'; // using aux as a wrapper for the adjacent tags || can React Fragment.
 
 import classes from './Layout.module.css' //module css
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+//setting class component to handle one click for side drawer
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
-export default layout
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        //since using set state we need to use prev state
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
--- DevCamp/Projects/burger-builder/src/components/Logo/Logo.js
+++ DevCamp/Projects/burger-builder/src/components/Logo/Logo.js
@@ -4,9 +4,10 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
 
 
-const Logo = props => (
-  <div className={classes.Logo}>
-    <img src={burgerLogo} alt="Burger Logo" />
+const logo = (props) => (
+  <div className={classes.Logo} style={{height: props.height}}>
+      <img src={burgerLogo} alt="MyBurger" />
   </div>
 );
-export default Logo;
+
+export default logo;
