195dcfd3fd00e6ec574f7c05712c735da013b80c
Jose Gonzalez <Gonzalezj@protonmail.com>
2019-07-29 21:12:38 -0400

    setup axios and get method. No catch implemented

--- DevCamp/HTTP&Ajax/src/components/Post/Post.js
+++ DevCamp/HTTP&Ajax/src/components/Post/Post.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
-        </div>
-    </article>
-);
+const post = (props) => {
+    const {title, author} = props.post
+    console.log(props);
+    
+    return (
+        <article className="Post" onClick={props.clicked}>
+            <h1>{title.substring(0, 10)}...</h1>
+            <div className="Info">
+                <div className="Author">{author}</div>
+            </div>
+        </article>
+    )
+}
+
 
 export default post;
--- DevCamp/HTTP&Ajax/src/containers/Blog/Blog.js
+++ DevCamp/HTTP&Ajax/src/containers/Blog/Blog.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+//root page
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    Axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      console.log(response);
+      const data = response.data.slice(0,4)
+      const updatedPost = data.map(post=>{
+          return {...post, author:'Joshi'}
+      })
+      this.setState({ posts: updatedPost }); //needs to be called in the "then" so that the data has been fetched
+    });
+  }
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post post={post}/>;
+    });
+    return (
+      <div>
+        <section className="Posts">
+        {posts}
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
