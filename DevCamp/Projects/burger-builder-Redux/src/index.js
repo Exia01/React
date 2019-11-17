3f7f340c4373cf7a3cbbf0bff9e923a4e6bf1492
Jose Gonzalez <Gonzalezj@protonmail.com>
2019-11-17 17:15:48 -0500

    add redux and setup provider

--- DevCamp/Projects/burger-builder-Redux/src/index.js
+++ DevCamp/Projects/burger-builder-Redux/src/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
-// wrapping on const ad passing back 
+import burgerReducer from './store/reducers/burger_reducer';
+
+const rootReducer = combineReducers({
+  brg: burgerReducer
+});
+
+const store = createStore(rootReducer); //if single reducer, can pass directly here
+
+// wrapping on const ad passing back
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    {/*Provider wraps up the browser router by standard?*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
-ReactDOM.render(
-  app,
-  document.getElementById('root')
-);
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
