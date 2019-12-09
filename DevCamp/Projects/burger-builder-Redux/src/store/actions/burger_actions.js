67c5cbf7c869a7c3ce3578b7f5f0160ce83f3db9
Jose Gonzalez <Gonzalezj@protonmail.com>
2019-12-08 20:58:37 -0500

    setup files, add reference

--- DevCamp/Projects/burger-builder-Redux/src/store/actions/burger_actions.js
+++ DevCamp/Projects/burger-builder-Redux/src/store/actions/burger_actions.js
@@ -4,8 +4,12 @@ import {
 } from '../constants/BurgerActionType';
 
 export const ADD_ING_HANDLER = (ignName = null) => {
-  return { type: ADD_INGREDIENT, payload: { ingredientName: ignName } };
+  return {type: ADD_INGREDIENT, payload: {ingredientName: ignName}};
 };
 export const REMOVE_ING_HANDLER = (ignName = null) => {
-  return { type: REMOVE_INGREDIENT, payload: { ingredientName: ignName  } };
+  return {type: REMOVE_INGREDIENT, payload: {ingredientName: ignName}};
 };
+
+
+
+// Organization:https://stackoverflow.com/questions/34965856/what-is-the-point-of-the-constants-in-redux
