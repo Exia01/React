8ccbe44d402a4c8cd89fa0b245646d6c6893f46b
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-01-05 18:47:32 -0500

    apply route guards

--- DevCamp/Authentication/burger-builder-Redux/src/App.js
+++ DevCamp/Authentication/burger-builder-Redux/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Layout from './hoc/Layout/Layout';
@@ -17,29 +17,47 @@ export class App extends Component {
     this.props.onTryAutoSignup();
   }
   render() {
+    let routes = (
+      <Switch>
+        ((
+        <Route path='/auth' component={Auth} />
+        ), (<Route path='/' exact component={BurgerBuilder} />
+        ),
+        <Redirect to='/' />
+        );
+      </Switch>
+    );
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/checkout' component={Checkout} />,
+          <Route exact path='/orders' component={Orders} />,
+          <Route path='/logout' component={Logout} />,
+          <Route path='/' exact component={BurgerBuilder} />,
+          <Redirect to='/' />
+        </Switch>
+      );
+    }
     return (
       <div>
-        <Layout>
-          <Switch>
-            <Route exact path='/orders' component={Orders} />
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/auth' component={Auth} />
-            <Route path='/logout' component={Logout} />
-            <Route path='/' exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
+        <Layout>{routes}</Layout>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.idToken !== null
+  };
+};
 const mapDispatchToProps = dispatch => {
   return {
     onTryAutoSignup: () => dispatch(authCheckState())
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App)); //helps passed the props down '''withRouter'''
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App)); //helps passed the props down '''withRouter'''
 
 // NOTE: Only BurgerBuilder itself gets the props not the nested component
 //REact Router:https://www.freecodecamp.org/news/a-guide-to-upgrading-to-react-router-4/
