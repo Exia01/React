82830f435a91deee3fdd8191ffa6a3a68cd45f32
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-01-08 17:43:22 -0500

    fixed ui bugs

--- DevCamp/Authentication/burger-builder-Redux/src/App.js
+++ DevCamp/Authentication/burger-builder-Redux/src/App.js
@@ -34,6 +34,7 @@ export class App extends Component {
           <Route exact path='/orders' component={Orders} />,
           <Route path='/logout' component={Logout} />,
           <Route path='/' exact component={BurgerBuilder} />,
+          <Route path='/auth' component={Auth} />
           <Redirect to='/' />
         </Switch>
       );
--- DevCamp/Authentication/burger-builder-Redux/src/components/Navigation/SideDrawer/SideDrawer.js
+++ DevCamp/Authentication/burger-builder-Redux/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,23 +4,23 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
- 
 
 //redux props coming from layout.
-const sideDrawer = props => { 
+const sideDrawer = props => {
   //check from the incoming prop
   let attachedClasses = [classes.SideDrawer, classes.Close];
   if (props.open) {
     attachedClasses = [classes.SideDrawer, classes.Open];
   }
-  
-  
+
   return (
     <React.Fragment>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={attachedClasses.join(' ')}>
+
+      {/*could also pass the props to the items instead of the div*/}
+      <div className={attachedClasses.join(' ')} onClick={props.closed}>
         <div className={classes.Logo}>
-          <Logo/>
+          <Logo />
         </div>
         <nav>
           <NavigationItems isAuthenticated={props.isAuth} />
