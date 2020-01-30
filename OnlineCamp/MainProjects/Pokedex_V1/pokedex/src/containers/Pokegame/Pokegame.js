07c561aec07bfa51183da272e6315dc8114c31bb
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-01-30 08:12:26 -0500

    update css Modules and pokegame

--- OnlineCamp/MainProjects/Pokedex_V1/pokedex/src/containers/Pokegame/Pokegame.js
+++ OnlineCamp/MainProjects/Pokedex_V1/pokedex/src/containers/Pokegame/Pokegame.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import Pokedex from '../../components/Pokedex/Pokedex';
+
 import PokeGameClasses from '../../cssModules/Pokegame.module.css';
 
-export class Pokegame extends Component {
+class Pokegame extends Component {
+  _isMounted = false;
   static defaultProps = {
     initialPokemons: [
       { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
@@ -16,32 +19,81 @@ export class Pokegame extends Component {
       { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
     ]
   };
+  state = {
+    newPokeList1: null,
+    newPokeList2: null
+  };
 
-  generateRandomPokemonCardCount = (count,pokemonList) => {
-      let newPokemonArr = []
-      while (count > 0) { //explicit just in case or could use a for loop
-          const startIndex = Math.floor(Math.random() * pokemonList.length)
-          const endIndex = startIndex+1
-          newPokemonArr.push(...pokemonList.slice(startIndex, endIndex))
-          count--
-        }
-        return newPokemonArr
-
+  generateRandomPokemonCardCount = (count, pokemonList) => {
+    let newPokemonArr = [];
+    while (count > 0) {
+      //explicit just in case or could use a for loop
+      const startIndex = Math.floor(Math.random() * pokemonList.length);
+      const endIndex = startIndex + 1;
+      newPokemonArr.push(...pokemonList.slice(startIndex, endIndex));
+      count--;
+    }
+    return newPokemonArr;
   };
 
   componentDidMount() {
-      let newPokeList = this.generateRandomPokemonCardCount(4,this.props.initialPokemons)
-      console.log(newPokeList)
+    this._isMounted = true; //  if (this._isMounted) { set state
+
+    if (this._isMounted) {
+      let newPokemonsObj = this.props.initialPokemons.map(pokemon=>{
+        let tempPokeObj = {...pokemon}
+        let id = pokemon.id
+        if(pokemon.id<10){
+          id = `00${pokemon.id}`
+        }
+        if(pokemon.id<100 && pokemon.id > 9){
+          id = `0${pokemon.id}`
+        }
+        tempPokeObj.image=`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`
+        return tempPokeObj
+      })
+      let newPokeList1;
+      let newPokeList2;
+      newPokeList1 = this.generateRandomPokemonCardCount(
+        4,
+        newPokemonsObj
+      );
+      newPokeList2 = this.generateRandomPokemonCardCount(
+        4,
+        this.props.initialPokemons
+      );
+      this.setState({ newPokeList1, newPokeList2 });
+
+    }
   }
 
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
   render() {
-    return <div className={PokeGameClasses.PokegameContainer}></div>;
+    let pokedexTag1;
+    let pokedexTag2;
+
+    if (this.state.newPokeList1 && this.state.newPokeList2) {
+      pokedexTag1 = <Pokedex pokeListObj={this.state.newPokeList1} />;
+      pokedexTag2 = <Pokedex pokeListObj={this.state.newPokeList2} />;
+    }
+
+    return (
+      <div className={PokeGameClasses.PokegameContainer}>
+        {pokedexTag1} {/*pokedexTag2*/}
+      </div>
+    );
   }
 }
 
 export default Pokegame;
 
-
 // Mutation resource: https://doesitmutate.xyz/
+
+// cancelling Axios call:https://github.com/axios/axios#cancellation
+
+// Static Definition for React:https://medium.com/front-end-weekly/understanding-static-in-javascript-10782149993
+
+//Difference between props and state:https://stackoverflow.com/questions/27991366/what-is-the-difference-between-state-and-props-in-react
