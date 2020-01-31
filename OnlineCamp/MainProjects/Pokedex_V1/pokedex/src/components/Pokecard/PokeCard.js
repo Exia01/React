5294be056ebe5e6de2adb2350629616612e765da
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-01-30 20:41:31 -0500

    update pokeCard component

--- OnlineCamp/MainProjects/Pokedex_V1/pokedex/src/components/Pokecard/PokeCard.js
+++ OnlineCamp/MainProjects/Pokedex_V1/pokedex/src/components/Pokecard/PokeCard.js
@@ -3,15 +3,14 @@ import React from 'react';
 import classes from '../../cssModules/Pokecard.module.css';
 
 const PokeCard = props => {
-  console.log(props)
   let { name, type, base_experience, id, image } = props.pokemon;
 
   return (
-    <article className={classes.PokeCard}>
+    <article className={classes.PokeCard} id={id}>
       <h2>{name}</h2>
       <img src={image} alt='Pokemon sprite' />
-      <p>type:{type}</p>
-      <p>EXP:{base_experience}</p>
+      <p>type:{type}<br/>
+      EXP:{base_experience}</p>
     </article>
   );
 };
