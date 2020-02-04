53108ea2bc06da098e9ea99bb78beaf1bfddca63
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-02-04 17:41:42 -0500

    update dependencies and components

--- OnlineCamp/4_introToStatePt1/src/App.js
+++ OnlineCamp/4_introToStatePt1/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import Game from "./Game";
-import Demo from "./Demo";
-import Button from "./Button";
-import BrokenClick from "./BrokenClick";
-import Rando from "./Rando";
-import Clicker from "./Clicker";
-import "./App.css";
+import React, { Component } from 'react';
+import Game from './Game';
+import Demo from './Demo';
+import Button from './Button';
+import BrokenClick from './BrokenClick';
+import Rando from './Rando';
+import Clicker from './Clicker';
+import './App.css';
 
 class App extends Component {
   render() {
