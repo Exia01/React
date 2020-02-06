199d24ad82bca5d5a4c6231beebb90a67463bc00
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-02-06 17:23:37 -0500

    complete  roll-dice app

--- OnlineCamp/MainProjects/roll-dice/src/component/Die.js
+++ OnlineCamp/MainProjects/roll-dice/src/component/Die.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-export const Die = () => {
-    return (
-        <div>
-            <h1>Die</h1>
-        </div>
-    )
-}
+const Die = props => {
+  console.log(props)
+  let rollingDiceNum = 'fas fa-dice-one';
+  let diceNumb;
+  if (props.faNum) {
+    rollingDiceNum = `fas fa-dice${props}`;
+  }
+  props.rollingDice ? (rollingDiceNum += ' shaking') : (diceNumb += ' ');
+
+  return (
+    <article>
+      <i className={rollingDiceNum}></i>
+    </article>
+  );
+};
+
+export default Die;
--- OnlineCamp/MainProjects/roll-dice/src/containers/RollDice.js
+++ OnlineCamp/MainProjects/roll-dice/src/containers/RollDice.js
@@ -1,13 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Die from '../component/Die';
 
 class RollDice extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Dice!</h1>
-            </div>
-        )
+  state = {
+    dices: {
+      1: 'fas fa-dice-one',
+      2: 'fas fa-dice-two',
+      3: 'fas fa-dice-three',
+      4: 'fas fa-dice-four',
+      6: 'fas fa-dice-five',
+      7: 'fas fa-dice-six'
+    },
+    diceCount: 2,
+    rollingDice: false
+  };
+
+  componentDidMount() {}
+
+  onClickDiceHandler = e => {
+      alert("Working")
+    this.setState({ rollingDice: true });
+    setTimeout(() => {
+        this.setState({rollingDice:false})
+    }, 2000);
+  };
+
+  randomizeRollNum = () => {
+    let randomNum = Math.floor(Math.random() * this.state.diceCount + 1);
+    return randomNum;
+  };
+
+  render() {
+    let diceTags = [];
+    let rollingCssClass;
+    for (let i = 0; i < this.state.diceCount; i++) {
+      diceTags.push(
+        <Die
+          onClickDiceHandler={this.onClickDiceHandler}
+          key={i}
+          rollingDice={this.state.rollingDice}
+        />
+      );
     }
+
+    return (
+      <div className='dice-container'>
+        {diceTags}
+        <button
+          onClick={this.onClickDiceHandler}
+          disabled={this.state.rollingDice}
+        >
+         {this.state.rollingDice? "Rolling.....":"Roll Dice"} 
+        </button>
+      </div>
+    );
+  }
 }
 
-export default RollDice
+export default RollDice;
