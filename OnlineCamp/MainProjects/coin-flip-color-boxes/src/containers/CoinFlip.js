5cd4a4413524c1cb96b7956cbaa5f36b67979952
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-02-09 19:56:17 -0500

    setup blank slate project

---
+++ OnlineCamp/MainProjects/coin-flip-color-boxes/src/containers/CoinFlip.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export class CoinFlip extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+
+export default CoinFlip
