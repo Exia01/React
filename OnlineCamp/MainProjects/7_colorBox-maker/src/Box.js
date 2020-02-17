71af7e3cdbc8e354839f97d4812b5eaef0062a3b
Jose Gonzalez <Gonzalezj@protonmail.com>
2020-02-17 15:51:12 -0500

    project setup

---
+++ OnlineCamp/MainProjects/7_colorBox-maker/src/Box.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Box = () => {
+  return (
+    <div className='ColorBox'>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default Box;
---
+++ OnlineCamp/MainProjects/7_colorBox-maker/src/BoxContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class BoxContainer extends Component {
+  state = {};
+  render() {
+    return <div><h1>Box Container</h1></div>;
+  }
+}
+
+export default BoxContainer;
