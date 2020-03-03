9dca79e39611396caace4d988e1834fdad89f788
Jose <gonzalezj@protonmail.com>
2020-03-03 17:04:45 -0500

    "update formatting"

--- OnlineCamp/MainProjects/12_dadJokesAppRefactor/src/Joke.js
+++ OnlineCamp/MainProjects/12_dadJokesAppRefactor/src/Joke.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import "./Joke.css";
-import { getColor, getEmoji } from "./utils";
+import React, { Component } from 'react';
+import './Joke.css';
+import { getColor, getEmoji } from './utils';
 
 const Joke = props => {
   const borderColorStyle = getColor(props.votes);
   const emoji = getEmoji(props.votes);
   return (
-    <div className="Joke">
-      <div className="Joke-buttons">
-        <i className="fas fa-arrow-up" onClick={props.upvote} />
-        <span className="Joke-votes" style={{ borderColor: borderColorStyle }}>
+    <div className='Joke'>
+      <div className='Joke-buttons'>
+        <i className='fas fa-arrow-up' onClick={props.upvote} />
+        <span className='Joke-votes' style={{ borderColor: borderColorStyle }}>
           {props.votes}
         </span>
-        <i className="fas fa-arrow-down" onClick={props.downvote} />
+        <i className='fas fa-arrow-down' onClick={props.downvote} />
       </div>
-      <div className="Joke-text">{props.text}</div>
-      <div className="Joke-smiley">
+      <div className='Joke-text'>{props.text}</div>
+      <div className='Joke-smiley'>
         <i className={emoji} />
       </div>
     </div>
