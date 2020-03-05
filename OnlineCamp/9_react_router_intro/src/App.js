ef9f61824cfb65397eb4599a0168f4639df28c56
Jose <gonzalezj@protonmail.com>
2020-03-05 18:47:45 -0500

    added comments

--- OnlineCamp/9_react_router_intro/src/App.js
+++ OnlineCamp/9_react_router_intro/src/App.js
@@ -2,12 +2,12 @@
 // IF YOU ARE LOOKING FOR THE 'fake' ROUTING EXAMPLE...
 // I MOVED THE CODE INTO FakeRouting.js
 // ====================================================
-import React, { Component } from "react";
-import { Route, Switch, NavLink } from "react-router-dom";
-import About from "./About";
-import Dog from "./Dog";
-import Contact from "./Contact";
-import "./App.css";
+import React, { Component } from 'react';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import About from './About';
+import Dog from './Dog';
+import Contact from './Contact';
+import './App.css';
 
 const Hater = () => <h1>I ABSOLUTELY HATE DOGS!</h1>;
 
@@ -16,13 +16,14 @@ class App extends Component {
     return (
       <div className='App'>
         <nav className='App-nav'>
-          <NavLink activeClassName='active-link' to='/'>
+          {/* Renders as an a tag, different from NavLink, which does not have active className */}
+          <NavLink exact activeClassName='active-link' to='/'>
             About
           </NavLink>
-          <NavLink activeClassName='active-link' to='/contact'>
+          <NavLink exact activeClassName='active-link' to='/contact'>
             Contact
           </NavLink>
-          <NavLink activeClassName='active-link' to='/dog'>
+          <NavLink exact activeClassName='active-link' to='/dog'>
             Dog
           </NavLink>
 
@@ -37,16 +38,20 @@ class App extends Component {
         </nav>
 
         <Switch>
+          {/* Switch renders one case or "Route" */}
           <Route exact path='/' component={About} />
+          {/* exact ensures no other route gets match  */}
           <Route exact path='/contact' component={Contact} />
           <Route exact path='/dog' component={Dog} />
           <Route exact path='/dog/hater' component={Hater} />
 
-          {/* THESE TWO ROUTES ARE FROM THE LAST VIDEO IN THE SECTION! */}
           {/* ======================================================== */}
-          {/* Component will instantiate a new Dog every time */}
+          {/* Renders two separate components with props */}
+          {/* Component will instantiate a new Dog every time can harm performance!!! */}
           <Route exact path='/dog/c' component={() => <Dog name='Muffins' />} />
-          {/* Render will re-use the existing dog component */}
+          {/* Render will re-use the existing dog component, better practice?  no remounting and mounting*/}
+
+          {/* if passing props it is preferable to use render */}
           <Route exact path='/dog/r' render={() => <Dog name='Biscuits' />} />
         </Switch>
       </div>
@@ -55,3 +60,6 @@ class App extends Component {
 }
 
 export default App;
+
+// Router Info
+// https://stackoverflow.com/questions/47879663/root-navlink-always-get-active-class-react-router-dom
--- OnlineCamp/9_react_router_intro/src/Dog.js
+++ OnlineCamp/9_react_router_intro/src/Dog.js
@@ -13,7 +13,7 @@ class Dog extends Component {
       <div className='Dog'>
         <h1>Dog!!!</h1>
         <h3>This dog is named: {this.props.name}</h3>
-        <img src='https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1320&q=80' />
+        <img src='https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1320&q=80' alt="Dog" />
       </div>
     );
   }
