b944dc2ff6b0020eee12d3f121fd915de8b32c99
Jose <gonzalezj@protonmail.com>
2020-03-09 17:52:59 -0400

    setup current layout

--- OnlineCamp/MainProjects/13_vendingMachine/src/App.js
+++ OnlineCamp/MainProjects/13_vendingMachine/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import VendingMachine from './containers/VendingMachine';
+import { Switch, Route } from 'react-router-dom';
+
+
+
+import Soda from './components/Soda';
+import FreshSardines from './components/FreshSardines';
+import Chips from './containers/Chips';
 
 function App() {
   return (
     <div className="App">
-   
+      <Switch>
+        <Route exact path='/' component={VendingMachine} />
+        <Route exact path='/Chips' component={Chips} />
+        {/*  could pass props and or just do component if not*/}
+        <Route exact path='/Soda' render={() => <Soda />} />
+        <Route exact path='/FreshSardines' render={() => <FreshSardines />} />
+      </Switch>
     </div>
   );
 }
--- OnlineCamp/MainProjects/13_vendingMachine/src/components/FreshSardines.js
+++ OnlineCamp/MainProjects/13_vendingMachine/src/components/FreshSardines.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const FreshSardines = (props) => {
+const FreshSardines = props => {
+    const backButtonClickHandler = () => {
+        let history
+        if (props.history) {
+              history = props.history
+        }
+        console.log(props.history);
+        // console.log(history)
+        // if(history) history.push('/home') else  this.props.history.goBack();
+        if (history !== null || history !== undefined)
+            history.push('/')
+        else
+            window.location.href = '/soda';
+    };
     return (
-        <div>
-            <h1>Fresh Sardines</h1>
-            
+        <div className='sardines'>
+            <div className='sardines-content-grid'>
+                <p>You don't eat Sardines. The Sardines eat you!! </p>
+                <div className='sardines-button-grid-item'>
+                    <button onClick={backButtonClickHandler} className='sardines-back-button'>Go Back</button>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default FreshSardines
+export default withRouter(FreshSardines);
