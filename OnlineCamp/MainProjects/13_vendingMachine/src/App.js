51c003f932215e8f1e6335aa0529482a7158bc23
Jose <gonzalezj@protonmail.com>
2020-03-09 18:04:32 -0400

    delete project remake app

--- OnlineCamp/MainProjects/13_vendingMachine/src/App.js
+++ OnlineCamp/MainProjects/13_vendingMachine/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './App.css';
-import VendingMachine from './containers/VendingMachine';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import Navbar from "./Navbar";
+import VendingMachine from "./VendingMachine";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-
-
-import Soda from './components/Soda';
-import FreshSardines from './components/FreshSardines';
-import Chips from './containers/Chips';
-
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={VendingMachine} />
-        <Route exact path='/Chips' component={Chips} />
-        {/*  could pass props and or just do component if not*/}
-        <Route exact path='/Soda' render={() => <Soda />} />
-        <Route exact path='/FreshSardines' render={() => <FreshSardines />} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' render={() => <VendingMachine />} />
+          <Route exact path='/soda' render={() => <Soda />} />
+          <Route exact path='/sardines' render={() => <Sardines />} />
+          <Route exact path='/chips' render={() => <Chips />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
