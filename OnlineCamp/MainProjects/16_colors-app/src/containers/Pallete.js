aaf2235de4ee9ef3261577b502eb3a0f6b81f917
Jose <gonzalezj@protonmail.com>
2020-03-19 20:37:48 -0400

    create react app, add pallete container

---
+++ OnlineCamp/MainProjects/16_colors-app/src/containers/Pallete.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Pallete extends Component {
+    render() {
+        return (
+            <div className="Pallete">
+                {/* Navbar goes here */}
+                <div className="Pallete-colors">
+                    {/* Color boxes */}
+
+                </div>
+                {/* Footer */}
+            </div>
+        )
+    }
+}
+
+export default Pallete
