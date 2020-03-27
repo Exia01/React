5d777e9efb0ff0163b1984c71d1618b84bd3173d
Jose <gonzalezj@protonmail.com>
2020-03-26 20:37:51 -0400

    Fix Pallete Route

--- OnlineCamp/MainProjects/16_colors-app/src/App.js
+++ OnlineCamp/MainProjects/16_colors-app/src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 
 import Palette from './containers/Palette/Palette';
 import './App.css';
 
-import seedColors from './utils/seedColors'
-import { generatePalette } from './utils/colorHelpers'
+import seedColors from './utils/seedColors';
+import { generatePalette } from './utils/colorHelpers';
 
 function App() {
+  // Takes id from param  could move into utils
+  const findPalette = id => {
+    const foundPalette = seedColors.find(palette => {
+      return palette.id === id;
+    });
+    // console.log(foundPalette);
+
+    // generates the full spectrum of colors
+    return generatePalette(foundPalette);
+  };
+
   return (
-    // <div className="App">
-    
-    < div >
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div >
+    <Switch>
+      <Route exact path='/' component={() => <h1>Palette List Goes here</h1>} />
+      {/* Passing route props and extracting param */}
+      <Route
+        exact
+        path='/palette/:id'
+        render={routeProps => (
+          <Palette palette={findPalette(routeProps.match.params.id)} />
+        )}
+      />
+    </Switch>
+    //   <div>
+    //     <Palette palette={generatePalette(seedColors[4])} />
+    // </div >
   );
 }
 
