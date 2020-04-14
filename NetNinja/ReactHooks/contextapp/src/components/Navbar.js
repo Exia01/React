f5fe0cb4f2cd12ea785b8352209504c5daf13491
Jose <gonzalezj@protonmail.com>
2020-04-13 21:13:33 -0400

    Add Context and Context Consumer to Navbar and BookList

--- NetNinja/ReactHooks/contextapp/src/components/Navbar.js
+++ NetNinja/ReactHooks/contextapp/src/components/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
-const Navbar = () => {
-  return (
-    <nav>
-      <h1>Context App</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  );
-};
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+class Navbar extends Component {
+  // importing the constant declared at top and attaching to the all the instances
+  render() {
+    return (
+      // ThemeContext.Consumer expects a function and can be used in a functional component
+      <ThemeContext.Consumer>
+        {/* Using the consumer and consume the context by using it inside the function */}
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark; //is it dark or light theme
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
 
 export default Navbar;
