de726792c0d1e924efc427e676a285b0aadabead
Jose <gonzalezj@protonmail.com>
2020-04-16 22:02:12 -0400

    Add useContext hook to components, update to functional components

--- NetNinja/ReactHooks/contextapp/src/components/Navbar.js
+++ NetNinja/ReactHooks/contextapp/src/components/Navbar.js
@@ -1,40 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
-class Navbar extends Component {
-  // importing the constant declared at top and attaching to the all the instances
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => (
-          <ThemeContext.Consumer>
-            {/* // ThemeContext.Consumer expects a function and can be used in a functional component */}
-            {/* Using the consumer and consume the context by using it inside the function */}
-            {(themeContext) => {
-              // Get access to the authContext
-              const { isAuth, toggleAuth } = authContext;
-              const { isLightTheme, light, dark } = themeContext;
-              const theme = isLightTheme ? light : dark; //is it dark or light theme
-              return (
-                <nav style={{ background: theme.ui, color: theme.syntax }}>
-                  <h1>Context App</h1>
-                  <div>
-                    {isAuth ? <p>Logged in</p> : <p>Logged Out</p>}
-                    <button onClick={toggleAuth}>Toggle here</button>
-                  </div>
-                  <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                  </ul>
-                </nav>
-              );
-            }}
-          </ThemeContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
-}
+
+// BEFORE
+// class Navbar extends Component {
+//   // importing the constant declared at top and attaching to the all the instances
+//   render() {
+//     return (
+//       <AuthContext.Consumer>
+//         {(authContext) => (
+//           <ThemeContext.Consumer>
+//             {/* // ThemeContext.Consumer expects a function and can be used in a functional component */}
+//             {/* Using the consumer and consume the context by using it inside the function */}
+//             {(themeContext) => {
+//               // Get access to the authContext
+//               const { isAuth, toggleAuth } = authContext;
+//               const { isLightTheme, light, dark } = themeContext;
+//               const theme = isLightTheme ? light : dark; //is it dark or light theme
+//               return (
+//                 <nav style={{ background: theme.ui, color: theme.syntax }}>
+//                   <h1>Context App</h1>
+//                   <div>
+//                     {isAuth ? <p>Logged in</p> : <p>Logged Out</p>}
+//                     <button onClick={toggleAuth}>Toggle here</button>
+//                   </div>
+//                   <ul>
+//                     <li>Home</li>
+//                     <li>About</li>
+//                     <li>Contact</li>
+//                   </ul>
+//                 </nav>
+//               );
+//             }}
+//           </ThemeContext.Consumer>
+//         )}
+//       </AuthContext.Consumer>
+//     );
+//   }
+// }
+
+const Navbar = () => {
+  // Destructuring
+  const { isLightTheme, light, dark } = useContext(ThemeContext); //can reuse useContext as many times as wee need to
+  const { isAuth, toggleAuth } = useContext(AuthContext); //useContext 'hook'
+  const theme = isLightTheme ? light : dark; //is it dark or light theme
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div>
+        {isAuth ? <p>Logged in</p> : <p>Logged Out</p>}
+        <button onClick={toggleAuth}>Toggle here</button>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
--- NetNinja/ReactHooks/contextapp/src/components/ThemeToggle.js
+++ NetNinja/ReactHooks/contextapp/src/components/ThemeToggle.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class ThemeToggle extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { toggleTheme } = this.context;
-    return (
-      <div>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-      </div>
-    );
-  }
+// BEFORE
+// class ThemeToggle extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { toggleTheme } = this.context;
+//     return (
+//       <div>
+//         <button onClick={toggleTheme}>Toggle Theme</button>
+//       </div>
+//     );
+//   }
+// }
+
+const ThemeToggle = () => {
+  // 'hooking' the ThemeContext with the useContext hook
+  const { toggleTheme } = useContext(ThemeContext)
+  return (
+    <div>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
 }
 
 export default ThemeToggle;
