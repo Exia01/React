066952bb97103cc49bea2c799b75fedc9643e2c7
Jose <gonzalezj@protonmail.com>
2020-05-10 14:55:04 -0400

    Add basic route transitions

--- OnlineCamp/MainProjects/16_colors-app/src/App.js
+++ OnlineCamp/MainProjects/16_colors-app/src/App.js
@@ -10,6 +10,8 @@ import { generatePalette } from './utils/colorHelpers';
 import SingleColorPalette from './containers/SinglePalette/SingleColorPalette';
 import NewPaletteForm from './containers/NewPaletteForm/NewPaletteForm';
 
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 function App() {
   const savedPalettes = JSON.parse(window.localStorage.getItem('palettes')); //parsing from string
   const [palettes, setPalettes] = useState(savedPalettes || seedColors);
@@ -41,60 +43,77 @@ function App() {
   }, [palettes]);
 
   return (
-    <Switch>
-      {/* leaving on top to prevent the second route triggering */}
-      {/* Passing routeProps to enable use in component */}
-      <Route
-        exact
-        path='/palette/new'
-        render={(routeProps) => (
-          <NewPaletteForm
-            savePalette={savePalette}
-            palettes={palettes}
-            {...routeProps}
-          />
-        )}
-      />
-      <Route
-        exact
-        path='/'
-        // passing routeProps to enable use in PaletteList
-        component={(routeProps) => (
-          <PaletteList
-            palettes={palettes}
-            deletePalette={deletePalette}
-            {...routeProps}
-          />
-        )}
-      />
-      {/* Passing route props and extracting param */}
-      <Route
-        exact
-        path='/palette/:id'
-        // route props enables the usage of the props from route
-        render={(routeProps) => (
-          <Palette palette={findPalette(routeProps.match.params.id)} />
-        )}
-      />
+    // route will always render the switch, enables transitions
+    // grabbing the location from props and passing to render
+    <Route render={({ location }) => (
+      <TransitionGroup>
+        <CSSTransition classNames="fade" timeout={500} key={location.key}>
+          {/* locations informs the switch of where we are on the app */}
+          <Switch location={location}>
 
-      <Route
-        exact
-        path='/palette/:paletteId/:colorId'
-        render={(routeProps) => (
-          <SingleColorPalette
-            // Passing that one whole palette list with all colors included
-            // Will isolate that specific color in the individual palette
-            palette={findPalette(routeProps.match.params.paletteId)}
-            //passing colorId to identify color, could also move to with router
-            colorId={routeProps.match.params.colorId}
-          //
-          />
-        )}
-      />
-    </Switch>
-    //   <div>
-    //     <Palette palette={generatePalette(seedColors[4])} />
-    // </div >
+            {/* leaving on top to prevent the second route triggering */}
+            {/* Passing routeProps to enable use in component */}
+            <Route
+              exact
+              path='/palette/new'
+              render={(routeProps) => (
+                // being added to all the components and give class to style transition
+                <div className="page">
+                  <NewPaletteForm
+                    savePalette={savePalette}
+                    palettes={palettes}
+                    {...routeProps}
+                  />
+                </div>
+              )}
+            />
+            <Route
+              exact
+              path='/'
+              // passing routeProps to enable use in PaletteList
+              component={(routeProps) => (
+                <div className="page">
+                  <PaletteList
+                    palettes={palettes}
+                    deletePalette={deletePalette}
+                    {...routeProps}
+                  />
+                </div>
+              )}
+            />
+            {/* Passing route props and extracting param */}
+            <Route
+              exact
+              path='/palette/:id'
+              // route props enables the usage of the props from route
+              render={(routeProps) => (
+                <Palette palette={findPalette(routeProps.match.params.id)} />
+              )}
+            />
+
+            <Route
+              exact
+              path='/palette/:paletteId/:colorId'
+              render={(routeProps) => (
+                <div className="page">
+                  <SingleColorPalette
+                    // Passing that one whole palette list with all colors included
+                    // Will isolate that specific color in the individual palette
+                    palette={findPalette(routeProps.match.params.paletteId)}
+                    //passing colorId to identify color, could also move to with router
+                    colorId={routeProps.match.params.colorId}
+                  //
+                  />
+                </div>
+              )}
+            />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+      //   <div>
+      //     <Palette palette={generatePalette(seedColors[4])} />
+      // </div >
+    )} />
   );
 }
 
