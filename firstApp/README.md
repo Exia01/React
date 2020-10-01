374c7ad0a38198d3bf8ca119744bbab7cb96d4e3
Jose <gonzalezj-@protonmail.com>
2020-09-30 21:49:32 -0400

    restructure folders

--- firstApp/README.md
+++
@@ -1,3 +0,0 @@
-Coding along with an instructional video with over 44 lessons from The Net Ninja. 
-
-Link: https://www.youtube.com/watch?v=OxIDLw0M-m0&list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG
--- firstApp/client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-/* This will target the div on the index.html that is located on the public folder */
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker(); /* This register worker essentially caches the application?   ...I think */
